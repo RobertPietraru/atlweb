4127d8d5f16668a7f7be6cec60c73c5e0305b07f
RobertPietraru <rob_piet@yahoo.com>
2025-03-28 14:55:51 +0200

    logging again

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,40 +1,19 @@
-import { pino, multistream } from "pino";
-import fs from "fs";
+import { pino } from "pino";
 
-// Ensure logs directory exists
-if (process.env.NODE_ENV === 'development') {
-    if (!fs.existsSync('./logs')) {
-        fs.mkdirSync('./logs');
-    }
-}
-
-// Create a writable stream for logging to a file
-const logFile = pino.destination('./logs/app.log');
-
-// Create a pretty-print stream for the console
 const prettyStream = pino.transport({
-    target: 'pino-pretty', // Pretty prints logs in the console
-    options: { colorize: true } // Adds colors to console logs
+    target: 'pino-pretty',
+    options: { colorize: true }
 });
-const streams = [
-    { stream: prettyStream },
-];
-if (process.env.NODE_ENV === 'development') {
-    streams.push({ stream: logFile });
-}
 
-// Create a logger with multiple streams
 const log = pino(
     { level: 'info' },
-    multistream(streams)
+    prettyStream
 );
 
-// Log unhandled exceptions
 process.on('uncaughtException', (error) => {
     log.fatal(error, 'Uncaught Exception');
 });
 
-// Log unhandled promise rejections
 process.on('unhandledRejection', (reason, promise) => {
     log.fatal({
         reason,
