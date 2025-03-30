ec8775e9acc8884bfd800d860f5ca1f79ee1e4d6
RobertPietraru <rob_piet@yahoo.com>
2025-03-30 13:10:49 +0300

    working editor

--- src/routes/(app)/course/[id]/chapter/[chapter_id]/lesson/[lesson_id]/exercise/[exercise_id]/+page.svelte
+++ src/routes/(app)/course/[id]/chapter/[chapter_id]/lesson/[lesson_id]/exercise/[exercise_id]/+page.svelte
@@ -1,8 +1,123 @@
 <script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import { marked } from 'marked';
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
 	let { data } = $props();
+
+	let htmlEditor: Monaco.editor.IStandaloneCodeEditor;
+	let cssEditor: Monaco.editor.IStandaloneCodeEditor;
+	let jsEditor: Monaco.editor.IStandaloneCodeEditor;
+	let monaco: typeof Monaco;
+	let htmlEditorContainer: HTMLElement | null = $state(null);
+	let cssEditorContainer: HTMLElement | null = $state(null);
+	let jsEditorContainer: HTMLElement | null = $state(null);
+
+    let activeTab : 'html' | 'css' | 'javascript' = $state('html');
+
+	const htmlSnippets = {
+		'!': {
+			prefix: '!',
+			body: [
+				'<!DOCTYPE html>',
+				'<html lang="en">',
+				'<head>',
+				'\t<meta charset="UTF-8">',
+				'\t<meta name="viewport" content="width=device-width, initial-scale=1.0">',
+				'\t<title>Document</title>',
+				'</head>',
+				'<body>',
+				'\t$0',
+				'</body>',
+				'</html>'
+			].join('\n')
+		},
+		div: {
+			prefix: 'div',
+			body: ['<div>', '\t$0', '</div>'].join('\n')
+		},
+		p: {
+			prefix: 'p',
+			body: ['<p>', '\t$0', '</p>'].join('\n')
+		},
+
+		span: {
+			prefix: 'span',
+			body: ['<span>', '\t$0', '</span>'].join('\n')
+		},
+		img: {
+			prefix: 'img',
+			body: ['<img src="$0" alt="Image">', '\t$0', '</img>'].join('\n')
+		},
+		br: {
+			prefix: 'br',
+			body: ['<br>'].join('\n')
+		},
+		hr: {
+			prefix: 'hr',
+			body: ['<hr>'].join('\n')
+		},
+		a: {
+			prefix: 'a',
+			body: ['<a href="$0">Link</a>'].join('\n')
+		},
+		ul: {
+			prefix: 'ul',
+			body: ['<ul>', '\t$0', '</ul>'].join('\n')
+		},
+		li: {
+			prefix: 'li',
+			body: ['<li>', '\t$0', '</li>'].join('\n')
+		},
+		ol: {
+			prefix: 'ol',
+			body: ['<ol>', '\t$0', '</ol>'].join('\n')
+		},
+		table: {
+			prefix: 'table',
+			body: ['<table>', '\t$0', '</table>'].join('\n')
+		}
+	};
+
+	onMount(async () => {
+		monaco = (await import('./monaco')).default;
+
+		htmlEditor = monaco.editor.create(htmlEditorContainer!, {
+			theme: 'vs-dark',
+			automaticLayout: true,
+            
+			language: 'html'
+		});
+
+		cssEditor = monaco.editor.create(cssEditorContainer!, {    
+			theme: 'vs-dark',
+			automaticLayout: true,
+			language: 'css'
+		});
+
+		jsEditor = monaco.editor.create(jsEditorContainer!, {
+			theme: 'vs-dark',
+			automaticLayout: true,
+			language: 'javascript'
+		});
+
+		const htmlModel = monaco.editor.createModel(data.exercise.initialHtml, 'html');
+		const cssModel = monaco.editor.createModel(data.exercise.initialCss, 'css');
+		const jsModel = monaco.editor.createModel(data.exercise.initialJavascript, 'javascript');
+
+		htmlEditor.setModel(htmlModel);
+		cssEditor.setModel(cssModel);
+		jsEditor.setModel(jsModel);
+	});
+
+	onDestroy(() => {
+		monaco?.editor.getModels().forEach((model) => model.dispose());
+		htmlEditor?.dispose();
+		cssEditor?.dispose();
+		jsEditor?.dispose();
+	});
 </script>
 
 <Resizable.PaneGroup direction="horizontal" class="">
@@ -35,13 +150,26 @@
 		</Resizable.PaneGroup>
 	</Resizable.Pane>
 	<Resizable.Handle withHandle class="bg-muted" />
-	<Resizable.Pane defaultSize={25}>
-		<div class="flex h-full items-center justify-center p-6">
-			<span class="font-semibold">Editor cod</span>
-		</div>
+	<Resizable.Pane defaultSize={60} class="p-0">
+		<Tabs.Root class="h-[calc(100vh-4rem)]" bind:value={activeTab}>
+			<Tabs.List class="flex justify-start border-b border-muted">
+				<Tabs.Trigger value="html" class="rounded-t-lg border border-b-0 border-muted px-4 py-2 text-sm font-medium hover:bg-muted/50 data-[state=active]:bg-background data-[state=active]:border-muted">HTML</Tabs.Trigger>
+				<Tabs.Trigger value="css" class="rounded-t-lg border border-b-0 border-muted px-4 py-2 text-sm font-medium hover:bg-muted/50 data-[state=active]:bg-background data-[state=active]:border-muted">CSS</Tabs.Trigger>
+				<Tabs.Trigger value="javascript" class="rounded-t-lg border border-b-0 border-muted px-4 py-2 text-sm font-medium hover:bg-muted/50 data-[state=active]:bg-background data-[state=active]:border-muted">JavaScript</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="html" class="h-full w-full">
+				<div bind:this={htmlEditorContainer} class="h-full w-full" id="html-editor"></div>
+			</Tabs.Content>
+			<Tabs.Content value="css" class="h-full w-full">
+				<div bind:this={cssEditorContainer} class="h-full w-full" id="css-editor"></div>
+			</Tabs.Content>
+			<Tabs.Content value="javascript" class="h-full w-full">
+				<div bind:this={jsEditorContainer} class="h-full w-full" id="javascript-editor"></div>
+			</Tabs.Content>
+		</Tabs.Root>
 	</Resizable.Pane>
 	<Resizable.Handle withHandle />
-	<Resizable.Pane defaultSize={50}>
+	<Resizable.Pane defaultSize={30}>
 		<div class="flex h-full items-center justify-center p-6">
 			<span class="font-semibold">Content</span>
 		</div>
