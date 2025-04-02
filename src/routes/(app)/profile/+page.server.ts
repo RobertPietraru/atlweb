3b8e5c79f43d4cf7d3616edd719a38319c97f1c3
RobertPietraru <rob_piet@yahoo.com>
2025-04-02 22:34:39 +0300

    minor fix + favicon

--- src/routes/(app)/profile/+page.server.ts
+++ src/routes/(app)/profile/+page.server.ts
@@ -1,8 +1,8 @@
 import { adminService } from "$lib/injection";
 
-export const load = async ({ locals, params }) => {
-    const page = params.page ? parseInt(params.page) : 1;
-    const limit = params.limit ? parseInt(params.limit) : 10;
+export const load = async ({ locals, url }) => {
+    const page = url.searchParams.get('page') ? parseInt(url.searchParams.get('page')!) : 1;
+    const limit = url.searchParams.get('limit') ? parseInt(url.searchParams.get('limit')!) : 10;
     const response = await adminService.getYourUnsolvedSubmissions(locals.user!.id, page, limit);
     return { user: response.submissions.map(submission => ({
         ...submission,
