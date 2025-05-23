cb68a34e91aeb728b7af47feda4ad67e3ce21dfd
RobertPietraru <rob_piet@yahoo.com>
2025-05-23 18:59:02 +0300

    Admin panel redesign

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,30 +1,49 @@
 <script lang="ts">
-	import { Card, CardContent, CardHeader, CardTitle } from '$lib/components/ui/card';
+	import {
+		ArrowUpRightIcon,
+		LibraryBig,
+		UsersRound
+	} from '@lucide/svelte';
+
 	let { data } = $props();
 </script>
 
-<h1 class="mb-8 text-3xl font-bold">Dashboard administrativ</h1>
+<h1 class="mb-8 text-3xl font-bold">Panou administrativ</h1>
 
-<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+<div class="grid gap-6 md:grid-cols-3">
 	{#if data.canViewAdminUsersPage}
-		<Card>
-			<CardHeader>
-				<CardTitle>Utilizatori</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<a href="/admin/users" class="text-blue-500 hover:underline">Gestionează utilizatorii</a>
-			</CardContent>
-		</Card>
+		<a
+			href="/admin/courses"
+			class="group flex h-full cursor-pointer flex-col overflow-hidden border-2 p-6 transition-all hover:scale-[1.02] hover:border-primary hover:shadow-lg"
+		>
+			<div class="flex justify-between">
+				<LibraryBig class="mb-4 h-12 w-12" />
+				<div
+					class="flex h-8 w-8 items-center justify-center rounded-full bg-primary/10 opacity-0 transition-opacity group-hover:opacity-100"
+				>
+					<ArrowUpRightIcon class="h-4 w-4 text-primary" />
+				</div>
+			</div>
+			<h2 class="mb-2 text-2xl font-semibold">Cursuri</h2>
+			<p class="text-muted-foreground">Gestionează cursurile</p>
+		</a>
 	{/if}
 
-	{#if data.canViewAdminCoursesPage}
-		<Card>
-			<CardHeader>
-				<CardTitle>Cursuri</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<a href="/admin/courses" class="text-blue-500 hover:underline">Gestionează cursurile</a>
-			</CardContent>
-		</Card>
+	{#if data.canViewAdminUsersPage}
+		<a
+			href="/admin/users"
+			class="group flex h-full cursor-pointer flex-col overflow-hidden border-2 p-6 transition-all hover:scale-[1.02] hover:border-primary hover:shadow-lg"
+		>
+			<div class="flex justify-between">
+				<UsersRound class="mb-4 h-12 w-12" />
+				<div
+					class="flex h-8 w-8 items-center justify-center rounded-full bg-primary/10 opacity-0 transition-opacity group-hover:opacity-100"
+				>
+					<ArrowUpRightIcon class="h-4 w-4 text-primary" />
+				</div>
+			</div>
+			<h2 class="mb-2 text-2xl font-semibold">Utilizatori</h2>
+			<p class="text-muted-foreground">Gestionează utilizatorii</p>
+		</a>
 	{/if}
 </div>
